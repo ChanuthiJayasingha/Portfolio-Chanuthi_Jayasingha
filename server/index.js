a800a8fd7efbc7d6203eba406b7415b50d8dd432
ChanuthiJayasingha <chanuthi.avindi@gmail.com>
2026-08-13 19:55:51 +0530

    Enable CORS on backend and update Contact API endpoint

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const cors = require('cors');
 
 // Allow requests from Vite frontend
 app.use(cors({
@@ -12,6 +13,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(cors());
 
 // Transporter configuration
 const transporter = nodemailer.createTransport({
